5841427367e57fb6ee604b6394ff943ccb30a548
tomasep22 <tomas.ep22@gmail.com>
2021-07-19 14:47:08 -0300

    Reorganize code

--- src/index.js
+++ src/index.js
@@ -3,103 +3,103 @@ import Icon from './img/coffee-beans.png'
 import contactTab from './contact.js'
 import Video from './Videos/Holding\ coffee\ beans.mp4'
 
-
 const HomeTab = (function() {
+    
+    // tab icon
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = Icon;
+    document.head.appendChild(link);
+
+    // display home tab
     function display() {
-    // Tab icon
-const link = document.createElement('link');
-link.rel = 'icon';
-link.href = Icon;
-document.head.appendChild(link);
-//
-const header = document.querySelector('header')
-// home video
-const backDiv = document.createElement('div');
-backDiv.classList.add('backDiv');
-const video = document.createElement('video');
-video.preload = 'auto'
-video.loop = 'true'
-video.autoplay = 'true'
-video.muted = 'true'
-video.width = window.innerWidth;
-backDiv.appendChild(video)
-const source = document.createElement('source')
-source.src = Video
-source.type = 'video/mp4'
-video.appendChild(source)
-document.addEventListener('load', () => {
-    video.play()
-})
-//
-const content = document.querySelector('#content');
-//
-content.appendChild(backDiv)
-//
+    const content = document.querySelector('#content');
+    // home video
+    const backDiv = document.createElement('div');
+    backDiv.classList.add('backDiv');
+    const video = document.createElement('video');
+    video.preload = 'auto'
+    video.loop = 'true'
+    video.autoplay = 'true'
+    video.muted = 'true'
+    video.width = window.innerWidth;
+    backDiv.appendChild(video)
+    const source = document.createElement('source')
+    source.src = Video
+    source.type = 'video/mp4'
+    video.appendChild(source)
+    document.addEventListener('load', () => {
+        video.play()
+    })
+    content.appendChild(backDiv)
 
-const h1 = document.createElement('h1');
-h1.classList.add('h1H');
-h1.textContent = 'WE HAVE YOU COVERED!';
-backDiv.appendChild(h1);
+    // h1 home
+    const h1H = document.createElement('h1');
+    h1H.classList.add('h1H');
+    h1H.textContent = 'WE HAVE YOU COVERED!';
+    backDiv.appendChild(h1H);
 
-const arrowDiv = document.createElement('div')
-arrowDiv.classList.add('arrowDiv')
-backDiv.appendChild(arrowDiv)
+    // arrow
+    const arrowDiv = document.createElement('div')
+    arrowDiv.classList.add('arrowDiv')
+    backDiv.appendChild(arrowDiv)
 
-const arrow = document.createElement('arrow')
-arrow.classList.add('arrow');
-arrowDiv.appendChild(arrow);
+    const arrow = document.createElement('arrow')
+    arrow.classList.add('arrow');
+    arrowDiv.appendChild(arrow);
 
-arrowDiv.addEventListener('click', () => {
-   window.scrollTo({top: window.innerHeight, left: 0, behavior: 'smooth'});
-})
+    arrowDiv.addEventListener('click', () => {
+    window.scrollTo({top: window.innerHeight, left: 0, behavior: 'smooth'});
+    })
 
+    // second div
+    const speechDiv = document.createElement('div')
+    speechDiv.classList.add('speech')
+    content.appendChild(speechDiv)
 
-const speechDiv = document.createElement('div')
-speechDiv.classList.add('speech')
-content.appendChild(speechDiv)
+    const p = document.createElement('p')
+    p.classList.add('speechP')
+    p.textContent = 'Whether it’s a celebration, a Sunday breakfast session, coffee with work mates or a break from shopping, we have you covered. Our baristas serve up our excellent blend of coffee and our waiters deliver your familiar favourites and our newest creations directly from the kitchen.'
+    speechDiv.appendChild(p);
 
-const p = document.createElement('p')
-p.classList.add('speechP')
-p.textContent = 'Whether it’s a celebration, a Sunday breakfast session, coffee with work mates or a break from shopping, we have you covered. Our baristas serve up our excellent blend of coffee and our waiters deliver your familiar favourites and our newest creations directly from the kitchen.'
-speechDiv.appendChild(p);
+    // third div
+    const emailFormDiv = document.createElement('div')
+    emailFormDiv.classList.add('emailFormDiv');
+    content.appendChild(emailFormDiv);
 
+    const h2EF = document.createElement('h2')
+    h2EF.classList.add('h2EF');
+    h2EF.textContent = 'Keep Updated';
+    emailFormDiv.appendChild(h2EF);
 
-const emailFormDiv = document.createElement('div')
-emailFormDiv.classList.add('emailFormDiv');
-content.appendChild(emailFormDiv);
+    const lineEF = document.createElement('hr');
+    lineEF.classList.add('lineEF');
+    emailFormDiv.appendChild(lineEF);
 
-const h2EF = document.createElement('h2')
-h2EF.classList.add('h2EF');
-h2EF.textContent = 'Keep Updated';
-emailFormDiv.appendChild(h2EF);
+    const emailForm = document.createElement('form')
+    emailForm.classList.add('emailForm');
+    emailFormDiv.appendChild(emailForm)
 
-const lineEF = document.createElement('hr');
-lineEF.classList.add('lineEF');
-emailFormDiv.appendChild(lineEF);
+    const pEF = document.createElement('p');
+    pEF.classList.add('pEF');
+    pEF.textContent = 'To be the first to hear of our exclusive news, offers and updates, please subscribe below.';
+    emailForm.appendChild(pEF);
 
+    const emailInput = document.createElement('input');
+    emailInput.type = 'email';
+    emailInput.classList.add('email');
+    emailInput.placeholder = 'Enter your email address';
+    emailForm.appendChild(emailInput);
 
-const emailForm = document.createElement('form')
-emailForm.classList.add('emailForm');
-emailFormDiv.appendChild(emailForm)
+    const submitEF = document.createElement('button');
+    submitEF.type = 'submit';
+    submitEF.disabled = true;
+    submitEF.textContent = 'Subscribe';
+    submitEF.classList.add('submitEF');
+    emailForm.appendChild(submitEF);
+}
 
-const pEF = document.createElement('p');
-pEF.classList.add('pEF');
-pEF.textContent = 'To be the first to hear of our exclusive news, offers and updates, please subscribe below.';
-emailForm.appendChild(pEF);
-
-const emailInput = document.createElement('input');
-emailInput.type = 'email';
-emailInput.classList.add('email');
-emailInput.placeholder = 'Enter your email address';
-emailForm.appendChild(emailInput);
-
-const submitEF = document.createElement('button');
-submitEF.type = 'submit';
-submitEF.disabled = true;
-submitEF.textContent = 'Subscribe';
-submitEF.classList.add('submitEF');
-emailForm.appendChild(submitEF);
-
+display();
 
 const footer = document.createElement('footer');
 document.body.appendChild(footer);
@@ -138,27 +138,28 @@ ulFoot.innerHTML = ATRIBUTTIONS.map((attribution, index) => {
     return `<li class='attributionLi attribution${index}'><a class='attribution' target='_blank' href='${attribution.link}'>${attribution.assett} ${attribution.author}</a></li>`
 }).join('');
 
-const homeLi = document.querySelector('#home');
-homeLi.addEventListener('click', displayHome)
 
+// display home tab
 function displayHome() {
     document.querySelector('#content').innerHTML = '';
-    document.body.removeChild(document.querySelector('footer'));
     display();
 }
-}
-display();
-window.onunload = function () {
-    window.scrollTo(0,0);
-};
 
-const contactLi = document.querySelector('#contact');
-contactLi.addEventListener('click', displayContact);
-
+// disĺay contact tab
 function displayContact() {
     content.innerHTML = '';
     contactTab.display();
 }
+
+// add events to tab
+const homeLi = document.querySelector('#home');
+homeLi.addEventListener('click', displayHome)
+const contactLi = document.querySelector('#contact');
+contactLi.addEventListener('click', displayContact);
+
+window.onunload = function () {
+    window.scrollTo(0,0);
+};
     return {}
 }());
 
